412b231b2bf7d904e0496956c766b24d6fb498c3
Laraib387 <khanmanokhanmano16@gmail.com>
2025-05-15 06:46:24 -0700

    Add files via upload
    Write a Python program that takes a person's age and categories it as:
    Child (<13)
    Teen (13–19)
    Adult (20–59)
    Senior (60+
    Write a Python program to check if a given number is positive, negative or zero .
    
    Write a Python program to:
    Print the first and last item of a number list
    Print the number in the list
    Write a Python program to Create a Dictionary and then update its value

---
+++ 2nd.py
@@ -0,0 +1,14 @@
+# write a python program to check if a number is positive, negetive or zero?
+
+num =int(input("enter the number:"))
+if num >10:
+    print("positve number")
+elif num==10:
+    print("zero")
+else :
+    print("negetive number")   
+
+
+
+
+
---
+++ 4rth.py
@@ -0,0 +1,17 @@
+# write a python program to creat a dictionary and then update its value?
+dict= {
+    "name":"Laraib",
+     "age":"17",
+      "City":"karachi",
+    
+}
+print("orignal dict:"),
+   #update value
+car="fortuner"
+dict.update({"car":car})
+print("updatr dict:",dict)
+
+
+
+
+
